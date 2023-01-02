893af8bc805895218d30d16220b5fd2a470b35c1
Attaporn Peungsook <aofattapon321@gmail.com>
2023-01-02 18:24:23 +0700

    change to signIn UI

--- lib/App.dart
+++ lib/App.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:jitd_client/src/BottomNavigationWidget.dart';
+// import 'package:jitd_client/src/BottomNavigationWidget.dart';
+import 'package:jitd_client/src/screens/SignIn.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomNavigationWidget(),
+      home: SignIn(),
     );
   }
 }
