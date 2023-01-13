adb819991d0a19b43accbb581fede1f770fbbb9e
Attaporn Peungsook <aofattapon321@gmail.com>
2023-01-13 21:44:51 +0700

    Modify Tutotial flowpagr
    - Modify Tutorial Page 1 - 5 to More Responsive

--- lib/src/screens/tutorials/TutorialPage5.dart
+++ lib/src/screens/tutorials/TutorialPage5.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jitd_client/src/BottomNavigationWidget.dart';
 import 'package:jitd_client/src/screens/tutorials/TutorialPage1.dart';
+import 'package:jitd_client/src/screens/tutorials/TutorialPage2.dart';
+import 'package:rive/rive.dart';
 
 import '../../constant.dart';
 
@@ -15,6 +18,7 @@ class _TutorialPage5State extends State<TutorialPage5> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           backgroundColor: primaryColorSubtle,
           elevation: 0,
@@ -23,102 +27,134 @@ class _TutorialPage5State extends State<TutorialPage5> {
         body: SafeArea(
           child: Stack(
             children: [
+              /// top-content
               Container(
                 height: MediaQuery.of(context).size.height * 0.54,
                 decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
-                    color: primaryColorSubtle
-                ),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50)),
+                    color: primaryColorSubtle),
                 child: Center(
-                  //color: Colors.red,
-                  child: Container(
-                    //color: Colors.orange,
-                    child: Column(
-                      children: [
-                        Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      /// JIT:D label
+                      SizedBox(
                           //color: Colors.green,
-                            child: const Padding(padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                              child: Text("JIT :D", style: TextStyle(color: Colors.white, fontSize: 64, fontWeight: FontWeight.bold)),)),
-                        Container(
-                          width: MediaQuery.of(context).size.width * 0.9,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(30),
-                              color: Colors.white
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 65),
-                            child: RichText(
-                              textAlign: TextAlign.center,
-                              text: const TextSpan(
-                                  text: 'สุดท้ายแล้ว เย้ มาทำแบบทดสอบกัน คือทดสอบที่ปรึกษา มาลองทำด้วยกันนะไม่ต้องเครียดด้วย ทำตามที่เป็นจริงๆนะคนเก่งของผมระดับผู้ให้คำปรึกษาสูง ก็จะ',
-                                  style: TextStyle(fontSize: 16, color: textColor1),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                        text: 'สามารถที่จะมองเห็นโพสได้มากขึ้น',
-                                        style: TextStyle(color: thirterydColor)),
-                                  ]
+                          height: MediaQuery.of(context).size.height * 0.1,
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 5),
+                            child: Text("JIT :D",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 64,
+                                    fontWeight: FontWeight.bold)),
+                          )),
+
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            /// content backgrround
+                            Align(
+                              alignment: const Alignment(0, -0.8),
+                              child: Container(
+                                margin: EdgeInsets.symmetric(
+                                    horizontal:
+                                        MediaQuery.of(context).size.height *
+                                            0.03),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: Colors.white),
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical:
+                                          MediaQuery.of(context).size.height *
+                                              0.02,
+                                      horizontal:
+                                          MediaQuery.of(context).size.width *
+                                              0.05),
+                                  child: RichText(
+                                    textAlign: TextAlign.center,
+                                    text: const TextSpan(
+                                        text:
+                                            "สุดท้ายแล้ว เย้ มาทำแบบทดสอบกัน คือทดสอบที่ปรึกษา มาลองทำด้วยกันนะ ไม่ต้องเครียดด้วย ทำตามที่เป็นจริงๆนะคนเก่งของผมระดับผู้ให้คำปรึกษาสูง ก็จะสามารถมองเห็นโพสได้มากยิ่งขึ้น",
+                                        style: TextStyle(
+                                            fontSize: 16, color: textColor1)),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+
+                            /// Bear
+                            Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  SizedBox(
+                                      // color: Colors.amber,
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.23,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      child: const RiveAnimation.asset(
+                                          "assets/images/login_screen_character.riv",
+                                          fit: BoxFit.cover,
+                                          animations: ['idle'])),
+                                ],
+                              ),
+                            )
+                          ],
                         ),
-                        Center(
-                          child: Container(
-                              margin: EdgeInsets.only(top: 32),
-                              child: Image.asset(
-                                'assets/images/bear.png',
-                                width: MediaQuery.of(context).size.width * 0.8,
-                                height: MediaQuery.of(context).size.width * 0.4,
-                                fit: BoxFit.cover,
-                              )
-                          ),
-                        )
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              // stack 4 on bottom
-              Center(
-                child: Align(
-                    alignment: AlignmentDirectional(0, 0.6),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const TutorialPage1()),
-                          );
-                        },
-                        child: Text("ข้ามเพื่อเริ่มการใช้งาน", style: TextStyle(fontWeight: FontWeight.bold)),
-                        style: ElevatedButton.styleFrom(
-                            primary: thirterydColor,
-                            minimumSize: const Size(100, 40),
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
-                          // fixedSize: Size.fromHeight(30)
-                        ))),
+
+              Align(
+                alignment: const Alignment(0, 0.6),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: thirteryColorSubtle,
+                        minimumSize: const Size(100, 40),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5))
+                        // fixedSize: Size.fromHeight(30)
+                        ),
+                    child: const Text("ข้ามเพื่อเริ่มการใช้งาน",
+                        style: TextStyle(fontWeight: FontWeight.bold))),
               ),
-              Center(
-                child: Align(
-                    alignment: AlignmentDirectional(0, 0.8),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const TutorialPage1()),
-                          );
+
+              Align(
+                alignment: const Alignment(0, 0.85),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) => const BottomNavigationWidget(),
+                        ),
+                        (r) {
+                          return false;
                         },
-                        child: Text("ทำแบบทดสอบ", style: TextStyle(fontWeight: FontWeight.bold)),
-                        style: ElevatedButton.styleFrom(
-                            primary: thirterydColor,
-                            minimumSize: const Size(100, 40),
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
-                          // fixedSize: Size.fromHeight(30)
-                        ))),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                        primary: thirterydColor,
+                        minimumSize: const Size(100, 40),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5))
+                        // fixedSize: Size.fromHeight(30)
+                        ),
+                    child: const Text("ถัดไป",
+                        style: TextStyle(fontWeight: FontWeight.bold))),
               ),
             ],
           ),
-        )
-    );
+        ));
   }
-
 }
-
